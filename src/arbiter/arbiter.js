1cdca147b7d17b9109bc4d1f66af90904022c592
Amirhossein Shahbabaee <amirhossein.shahbabaee@citynetwork.eu>
2023-02-24 13:10:45 +0100

    Bishop

--- src/arbiter/arbiter.js
+++ src/arbiter/arbiter.js
@@ -1,10 +1,12 @@
-import { getKnightMoves, getRookMoves } from './getMoves'
+import { getKnightMoves, getRookMoves, getBishopMoves } from './getMoves'
 
 const arbiter = {
 
     getRegularMoves : function ({position,piece,rank,file}) {
         if (piece.endsWith('n'))
             return getKnightMoves({position,rank,file});
+        if (piece.endsWith('b'))
+            return getBishopMoves({position,piece,rank,file});
         if (piece.endsWith('r'))
             return getRookMoves({position,piece,rank,file});
     
--- src/arbiter/getMoves.js
+++ src/arbiter/getMoves.js
@@ -52,3 +52,34 @@ export const getKnightMoves = ({position,rank,file}) => {
     })
     return moves
 }
+
+export const getBishopMoves = ({position,piece,rank,file}) => {
+    const moves = []
+    const us = piece[0]
+    const enemy = us === 'w' ? 'b' : 'w'
+
+    const direction = [
+        [-1,-1],
+        [-1,1],
+        [1,-1],
+        [1,1],
+    ]
+
+    direction.forEach(dir => {
+        for (let i = 1; i <= 8; i++) {
+            const x = rank+(i*dir[0])
+            const y = file+(i*dir[1])
+            if(position?.[x]?.[y] === undefined)
+                break
+            if(position[x][y].startsWith(enemy)){
+                moves.push ([x,y])
+                break;
+            }
+            if(position[x][y].startsWith(us)){
+                break
+            }
+            moves.push ([x,y])
+        }
+    })
+    return moves
+}
